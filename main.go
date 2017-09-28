0caea39bd8415a39e78cf92a105ccf9dc06c83b8
mikrob <mikael.robert@clicrdv.com>
2017-09-28 18:38:08 +0200

    store mail sent + code cleanup

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"sync"
+	"time"
 
 	context "golang.org/x/net/context"
 
@@ -18,6 +18,10 @@ import (
 )
 
 type EVENT string
+
+var (
+	esClient *es.ElasticsearchClient
+)
 
 const (
 	SENT        = EVENT("SENT")      // SENT BY CLICRDV MAIL MICROSERVICE
@@ -32,22 +36,25 @@ const (
 	UNSUBSCRIBE = EVENT("UNSUBSCRIBE")
 )
 
-type SendgridEvent []struct {
-	Email       string   `json:"email"`
-	Timestamp   int      `json:"timestamp"`
-	SMTPID      string   `json:"smtp-id"`
-	Event       string   `json:"event"`
-	Category    []string `json:"category"` // trouver un moyen de gerer une ou n category
-	SgEventID   string   `json:"sg_event_id"`
-	SgMessageID string   `json:"sg_message_id"`
-	Response    string   `json:"response,omitempty"`
-	Attempt     string   `json:"attempt,omitempty"`
-	Useragent   string   `json:"useragent,omitempty"`
-	IP          string   `json:"ip,omitempty"`
-	URL         string   `json:"url,omitempty"`
-	Reason      string   `json:"reason,omitempty"`
-	Status      string   `json:"status,omitempty"`
-	AsmGroupID  int      `json:"asm_group_id,omitempty"`
+type SendgridEvent struct {
+	Email       string    `json:"email"`
+	Timestamp   int64     `json:"timestamp"`
+	Date        time.Time `json:"@timestamp"`
+	SMTPID      string    `json:"smtp-id"`
+	Event       string    `json:"event"`
+	Category    []string  `json:"category"` // trouver un moyen de gerer une ou n category
+	SgEventID   string    `json:"sg_event_id"`
+	SgMessageID string    `json:"sg_message_id"`
+	Response    string    `json:"response,omitempty"`
+	Attempt     string    `json:"attempt,omitempty"`
+	Useragent   string    `json:"useragent,omitempty"`
+	IP          string    `json:"ip,omitempty"`
+	URL         string    `json:"url,omitempty"`
+	Reason      string    `json:"reason,omitempty"`
+	Status      string    `json:"status,omitempty"`
+	AsmGroupID  int       `json:"asm_group_id,omitempty"`
+	ClicRdvId   string    `json:"clicrdvid"'`
+	Groupid     string
 }
 
 type GrpcServer struct{}
@@ -55,16 +62,34 @@ type GrpcServer struct{}
 func (s *GrpcServer) NotifySentMail(ctx context.Context, followMail *pb.ClicRdvFollowMail) (*pb.SendMailStatus, error) {
 	log.Print("Received GRPC Call")
 	log.Printf("Received arguments : %s, %s", followMail.GetEmail(), followMail.GetUuid())
+	categories := []string{"MS-MAIL"}
+	sgEv := SendgridEvent{
+		Category:  categories,
+		Email:     followMail.GetEmail(),
+		Event:     followMail.GetEvent(),
+		ClicRdvId: followMail.GetUuid(),
+		Groupid:   followMail.GetGroupId(),
+		Timestamp: time.Now().Unix(),
+	}
+
+	StoreToEs(esClient, &sgEv)
 	return &pb.SendMailStatus{}, nil
 }
 
+func StoreToEs(esClient *es.ElasticsearchClient, event *SendgridEvent) {
+	event.Date = time.Unix(event.Timestamp, 0)
+	log.Println("Timestamp is :", event.Timestamp)
+	log.Println("Converted Timestamp is :", time.Unix(event.Timestamp, 0))
+	esClient.StoreJson(event)
+}
+
 func main() {
+	esClient = es.NewElasticsearchClient(os.Getenv("ES_URL"), "mail")
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
-		esClient := es.NewElasticsearchClient(os.Getenv("ES_URL"), "mail")
 		router := gin.Default()
-		router.POST("/v1/eventhook/", incomingWebhook(&esClient))
+		router.POST("/v1/eventhook/", incomingWebhook(esClient))
 		router.GET("/v1/mails", listMails)
 		router.GET("/v1/mail/:uuid", mailStates)
 		router.Run("0.0.0.0:3001")
@@ -73,7 +98,7 @@ func main() {
 	wg.Add(1)
 	go func() {
 		log.Print("Starting microservice grpc listening on 50053")
-		lis, err := net.Listen("tcp", "localhost:50053")
+		lis, err := net.Listen("tcp", "0.0.0.0:50053")
 		if err != nil {
 			log.Fatalf("Failed to listen : %v", err)
 		}
@@ -92,18 +117,20 @@ func incomingWebhook(esClient *es.ElasticsearchClient) func(c *gin.Context) {
 		// fmt.Println(c.Request.Header)
 		//	fmt.Printf("%s", string(body))
 		// fmt.Println("EVENT RECEIVED : ")
-		var events SendgridEvent
+		var events []SendgridEvent
 		if err := c.BindJSON(&events); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error":  "json decoding : " + err.Error(),
 				"status": http.StatusBadRequest,
 			})
-			fmt.Println(err.Error())
+			log.Println(err.Error())
 			return
 		}
 		for _, event := range events {
-			fmt.Printf("%+v\n", event)
-			esClient.StoreJson(event)
+			StoreToEs(esClient, &event)
+			log.Printf("%+v\n", event)
+			log.Println("email is : ", event.Email)
+			log.Println("Event is  : ", event.Event)
 		}
 		c.JSON(http.StatusOK, gin.H{"status": "ok"})
 		return
